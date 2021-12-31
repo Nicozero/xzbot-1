474594c667c4126a2feea5b019e6dce807582b8c
Nicozero <88931086+Nicozero@users.noreply.github.com>
2022-01-01 04:27:30 +0800

    Update cog.py

--- cogs/Radio/cog.py
+++ cogs/Radio/cog.py
@@ -1,6 +1,5 @@
 from disscord.ext import commands
-from .Radio import RadioView
-
+from .Radio_View import RadioView
 
 class RadioCog(commands.Cog, name="Radio"):
 
@@ -14,9 +13,10 @@ class RadioCog(commands.Cog, name="Radio"):
         print("Radio Button view added")
 
     @commands.command()
-    async def roles(self, ctx: commands.Context):
-        await ctx.send("Click a button to add or remove a role.", view=RadioView())
-
+    async def play(self, ctx: commands.Context):
+        embed=discord.Embed(title="Radio Station", description="Select station to start the radio", color=0xff0000)
+        embed.set_author(name="Asia Dream Radio", url="https://asiadreamradio.torontocast.stream/", icon_url="https://cdn.discordapp.com/attachments/613417127143014520/926569774824181791/Asia_Dream_Radio.png")
+        await ctx.send(embed=embed, view=RadioView())
 
 # setup functions for bot
 def setup(bot):
