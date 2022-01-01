066e47fe59995dafbcdcdd7d4b3e6369b418bf7a
Nicozero <88931086+Nicozero@users.noreply.github.com>
2022-01-01 22:52:21 +0800

    Update cog.py

--- cogs/radio/cog.py
+++ cogs/radio/cog.py
@@ -16,13 +16,13 @@ class RadioCog(commands.Cog, name="Radio"):
         print("Button view added")
 
     @commands.command(aliases=["p","radio"])
-    async def play(self, ctx: commands.Context):
+    async def play(self, ctx: commands.context):
         embed=discord.Embed(title="Radio Station", description="Select station to start the radio", color=0xff0000)
         embed.set_author(name="Asia Dream Radio", url="https://asiadreamradio.torontocast.stream/", icon_url="https://cdn.discordapp.com/attachments/613417127143014520/926569774824181791/Asia_Dream_Radio.png")
         await ctx.send(embed=embed, view=RadioView())
 
     @commands.command(aliases=["currentsong","np","nowplaying"])
-    async def cs(self, ctx: commands.Context):
+    async def cs(self, ctx: commands.context):
         embed=discord.Embed(title="Radio Station", description="Select station to see current song ", color=0xff0000)
         embed.set_author(name="Asia Dream Radio", url="https://asiadreamradio.torontocast.stream/", icon_url="https://cdn.discordapp.com/attachments/613417127143014520/926569774824181791/Asia_Dream_Radio.png")
         await ctx.send(embed=embed, view=csView())
