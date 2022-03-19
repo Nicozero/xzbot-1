3a781f004e67c48682407e9b726538d75017b7dd
Nicozero <88931086+Nicozero@users.noreply.github.com>
2022-03-20 00:37:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,16 +20,6 @@ def main():
     for folder in os.listdir("cogs"):
         if os.path.exists(os.path.join("cogs", folder, "cog.py")):
             client.load_extension(f"cogs.{folder}.cog")
-            
-    @client.command()
-    async def ping(ctx):
-        await ctx.send(f'Pong! {round (client.latency * 1000)} ms')
-
-    @client.event
-    async def on_message(message):
-        if "https://media.discordapp.net" in message.content :
-            x = message.content.replace("https://media.discordapp.net" , "https://cdn.discordapp.com")
-            await message.reply(x)
 
     # run the bot
     client.run(config.BOT_TOKEN)
