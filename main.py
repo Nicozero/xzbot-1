f666ec5afe62f56f25ab17e7e45e015a43f3738c
Nicozero <88931086+Nicozero@users.noreply.github.com>
2022-03-20 00:50:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,23 +11,33 @@ async def main():
     intents.members = True
 
     client = commands.Bot(command_prefix=config.PREFIX, intents=intents)
-
-    @client.event
-    async def on_ready():
-        print(f"{client.user.name} has connected to Discord.")
-
-    # load all cogs
-    async def load_extensions():
-        for folder in os.listdir("cogs"):
-            if os.path.exists(os.path.join("cogs", folder, "cog.py")):
-                await client.load_extension(f"cogs.{folder}.cog")
-
-    # run the bot
+    
+        # run the bot
     async with client:
         await load_extensions()
         await client.start(config.BOT_TOKEN)
 
 asyncio.run(main())
+
+    @client.event
+    async def on_ready():
+        print(f"{client.user.name} has connected to Discord.")
+    
+    # load all cogs
+    async def load_extensions():
+        for folder in os.listdir("cogs"):
+            if os.path.exists(os.path.join("cogs", folder, "cog.py")):
+                await client.load_extension(f"cogs.{folder}.cog")
+                
+    async def on_message(message):
+        if "https://media.discordapp.net" in message.content :
+            x = message.content.replace("https://media.discordapp.net" , "https://cdn.discordapp.com")
+        await message.reply(x)
+        
+    @client.command()
+    async def ping(ctx):
+        await ctx.send(f'Pong! {round (client.latency * 1000)} ms')
+
 
 if __name__ == "__main__":
     main()
