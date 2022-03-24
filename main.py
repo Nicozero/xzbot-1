454b94e19a0ed62bede124c23211f595ff914070
Nicozero <88931086+Nicozero@users.noreply.github.com>
2022-03-25 04:42:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ client = commands.Bot(command_prefix="..")
 @client.event
 async def on_ready():
   print("ready")
-  chnl = client.get_channel(os.getenv("logChannel")) 
+  chnl = client.get_channel(int(os.getenv("logChannel"))) 
   today = datetime.datetime.now()
   switchTime = today + datetime.timedelta(days=10)
   sleepTime = (switchTime.timestamp() - today.timestamp())
   heroku_conn = heroku3.from_key(os.getenv("KEY"))
   app = heroku_conn.app('xzbot-0')
   app.process_formation()['worker'].scale(0)
-  await chnl.send("xzbot-0 OFF ")
+  await chnl.send("xzbot-0 OFF \nLog Ready ")
   await asyncio.sleep(sleepTime)
   await chnl.send("switch xzbot-1 > xzbot-0")
   app.process_formation()['worker'].scale(1)
@@ -32,8 +32,11 @@ async def on_message_delete(msg):
 
 @client.event
 async def on_message(msg):
+  if msg.author.bot: return
   if "https://media.discordapp.net/" in msg.content:
-    msg.reply(msg.content.replace("https://media.discordapp.net/","https://cdn.discordapp.com/"))
+    await msg.reply(msg.content.replace("https://media.discordapp.net/","https://cdn.discordapp.com/"))
+    return
+  await client.process_commands(msg)
        
 @client.command()
 async def ping(ctx):
