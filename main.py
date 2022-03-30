9c22398a8d666acf6b4f97d42b062cf3a3012add
Nicozero <88931086+Nicozero@users.noreply.github.com>
2022-03-30 18:46:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,14 +34,14 @@ async def on_message_delete(msg):
 async def on_message(msg):
   if msg.author.bot: return
   if "https://media.discordapp.net/" in msg.content:
-  	output = ""
+  newmsg = ""
 	x = msg.split()
 	r = re.compile('https?.*?\.mp?4$')
 	output = list(filter(r.match, x))
-	for i in output:
-		output += str(i)+ "\n"
-    await msg.reply(output.replace("https://media.discordapp.net/", "https://cdn.discordapp.com/"))
-    return
+		for i in output:
+			newmsg += str(i)+ "\n"
+    		await msg.reply("link fix \n" + newmsg.replace("https://media.discordapp.net/", "https://cdn.discordapp.com/"))
+    		return
   await client.process_commands(msg)
        
 @client.command()
